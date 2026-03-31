99a739f62695dfa6b8bb905021e5bb0d38a917d2
hathen <hamanhth2005@gmail.com>
2026-03-31 09:32:52 +0700

    fix: ignore file xlsx

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from mapping import map_data
 from validate import validate_data
 
 def main():
-    df = clean_data("data/raw/7020-NK-Th12.2025.xlsx")
+    df = clean_data("data/raw/")
     df = map_data(df, "data/master/master_product.xlsx")
     validate_data(df)
     df.to_excel("output/final_clean.xlsx", index=False)
